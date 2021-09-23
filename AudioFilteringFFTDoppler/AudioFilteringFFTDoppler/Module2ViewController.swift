4c3aecdf2ef0e4a3fcc157ae34471246cda6056d
Nick <nlarsen@smu.edu>
2021-09-22 19:47:38 -0500

    Add ViewControllers and properties.

---
+++ AudioFilteringFFTDoppler/AudioFilteringFFTDoppler/Module2ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  Module2ViewController.swift
+//  AudioFilteringFFTDoppler
+//
+//  Created by Nicholas Larsen on 9/22/21.
+//
+
+import UIKit
+
+class Module2ViewController: UIViewController {
+
+    @IBOutlet weak var graphView: UIView!
+    @IBOutlet weak var handResultLabel: UILabel!
+    @IBOutlet weak var hzLabel: UILabel!
+    @IBOutlet weak var hzSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
