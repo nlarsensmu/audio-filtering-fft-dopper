bf617a8f5dfb16f57d4ff2021ff60ea0d1b0a18d
Nick <nlarsen@smu.edu>
2021-09-22 20:18:12 -0500

    Set up slider to adujst label.

--- AudioFilteringFFTDoppler/AudioFilteringFFTDoppler/Module2ViewController.swift
+++ AudioFilteringFFTDoppler/AudioFilteringFFTDoppler/Module2ViewController.swift
@@ -9,18 +9,34 @@ import UIKit
 
 class Module2ViewController: UIViewController {
 
+    
     @IBOutlet weak var graphView: UIView!
-    @IBOutlet weak var handResultLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var hzLabel: UILabel!
     @IBOutlet weak var hzSlider: UISlider!
     
+    let MIN_VALUE:Float = 10.0
+    let MAX_VALUE:Float = 15.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        DispatchQueue.main.async {
+            self.hzSlider.minimumValue = Float(self.MIN_VALUE)
+            self.hzSlider.maximumValue = Float(self.MAX_VALUE)
+            self.hzSlider.value = self.MIN_VALUE
+            self.hzLabel.text = String(format: "%.2lf Hz", self.MIN_VALUE)
+        }
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func sliderAction(_ sender: Any) {
+        DispatchQueue.main.async {
+            self.hzLabel.text = String(format: "%.2lf Hz", self.hzSlider.value)
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
