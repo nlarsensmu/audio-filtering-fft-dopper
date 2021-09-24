ab4371c8de9bbe7f7ef46b11fbe67ccc99687c0f
Steven Larsen <stevenlarsen@Stevens-Mini.attlocal.net>
2021-09-24 18:54:34 -0500

    I think I am close

--- AudioFilteringFFTDoppler/AudioFilteringFFTDoppler/Module1ViewController.swift
+++ AudioFilteringFFTDoppler/AudioFilteringFFTDoppler/Module1ViewController.swift
@@ -9,16 +9,43 @@ import UIKit
 
 class Module1ViewController: UIViewController {
 
+    //MARK: Outlets
     @IBOutlet weak var hz1: UILabel!
     @IBOutlet weak var hz2: UILabel!
     @IBOutlet weak var graphView: UIView!
     
+    
+    //MARK: Static setup
+    struct AudioConstants{
+        static let AUDIO_BUFFER_SIZE = 1024*4
+    }
+    let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)
+    lazy var graph:MetalGraph? = {
+        return MetalGraph(mainView: self.graphView)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        graph?.addGraph(withName: "fft",
+                        shouldNormalize: true,
+                        numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE/2)
 
-        // Do any additional setup after loading the view.
+        audio.startMicrophoneProcessing(withFps: 10.0)
+        
+        
+        Timer.scheduledTimer(timeInterval: 0.05, target: self,
+            selector: #selector(self.updateGraph),
+            userInfo: nil,
+            repeats: true)
     }
     
+    @objc
+    func updateGraph(){
+        self.graph?.updateGraph(
+            data: self.audio.fftData,
+            forKey: "fft"
+       )
+    }
 
     /*
     // MARK: - Navigation
