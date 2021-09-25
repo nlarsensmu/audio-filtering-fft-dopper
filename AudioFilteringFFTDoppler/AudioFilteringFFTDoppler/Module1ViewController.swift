4e4c794a561a83927b4f308ff6c9218315667a5f
Steven Larsen <stevenlarsen@Stevens-Mini.attlocal.net>
2021-09-25 18:02:58 -0500

    Changed the approach.  Keeping a set number of maxes and if they don't change enough then I am locking it

--- AudioFilteringFFTDoppler/AudioFilteringFFTDoppler/Module1ViewController.swift
+++ AudioFilteringFFTDoppler/AudioFilteringFFTDoppler/Module1ViewController.swift
@@ -23,15 +23,24 @@ class Module1ViewController: UIViewController {
     //MARK: Setup
     struct AudioConstants{
         static let AUDIO_BUFFER_SIZE = 1024
-        static let AUDIO_PROCESSING_HERTZ = 10.0
+        static let AUDIO_PROCESSING_HERTZ = 100.0
+        static let LOCKING_ITERATION_NUMBERS = 30
+        static let WINDOW_SIZE = 5
+        static let AMOUNT_KEPT_MAXES = 10
     }
     let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)
     lazy var graph:MetalGraph? = {
         return MetalGraph(mainView: self.view)
     }()
+    var currentIndex = 0
+    var max1: [Int] = [Int].init(repeating: -999999, count: AudioConstants.AMOUNT_KEPT_MAXES)
+    var max2: [Int] = [Int].init(repeating: -999999, count: AudioConstants.AMOUNT_KEPT_MAXES)
+        
+    // Old method
     var lastMaxIndex1 = -1
     var lastMaxIndex2 = -1
     var noChangeCount = 0
+    
     var locked:Bool = false
     
     override func viewDidLoad() {
@@ -59,9 +68,19 @@ class Module1ViewController: UIViewController {
     
     @objc
     func updateGraph(){
-        let indicies = audio.windowedMaxFor(nums: audio.fftData, windowSize: 10)
+        //typical update graph functions for debugging
+        self.graph?.updateGraph(
+            data: self.audio.fftData,
+            forKey: "fft"
+        )
+        self.graph?.updateGraph(
+            data: self.audio.timeData,
+            forKey: "time"
+        )
+        
+        //Update the labelsry
+        let indicies = audio.windowedMaxFor(nums: audio.fftData, windowSize: AudioConstants.WINDOW_SIZE)
         let peaks = audio.getTopIndices(indices: indicies, nums: audio.fftData)
-        let mean = vDSP.mean(audio.fftData)
         if !self.locked {
             DispatchQueue.main.async {
                 if let peak1:Int = peaks[0] as Int?,
@@ -72,19 +91,36 @@ class Module1ViewController: UIViewController {
                 }
             }
         }
-        self.graph?.updateGraph(
-            data: self.audio.fftData,
-            forKey: "fft"
-       )
-        self.graph?.updateGraph(
-            data: self.audio.timeData,
-            forKey: "time"
-        )
         
+        //add to the arrays we've made
+        self.max1[self.currentIndex] = peaks[0]
+        self.max2[self.currentIndex] = peaks[1]
+        currentIndex += 1
+        if currentIndex == AudioConstants.AMOUNT_KEPT_MAXES{
+            currentIndex = 0
+        }
+        var max1MatchCount = 0
+        for max in max1 {
+            if peaks[0] == max{
+                max1MatchCount += 1
+            }
+        }
+        var max2MatchCount = 0
+        for max in max2 {
+            if peaks[1] == max{
+                max2MatchCount += 1
+            }
+        }
+        
+        if max1MatchCount >= Int(Float(AudioConstants.AMOUNT_KEPT_MAXES) * 0.5) && max2MatchCount >= Int(Float(AudioConstants.AMOUNT_KEPT_MAXES) * 0.5) {
+            self.locked = true
+            self.lockedLabel.text = "Locked"
+        }
+        /*
         if peaks[0] != peaks[1] {
             if peaks[0] == self.lastMaxIndex1 && peaks[1] == self.lastMaxIndex2 && self.locked == false{
                 self.noChangeCount += 1
-                if noChangeCount == 3{
+                if noChangeCount == AudioConstants.LOCKING_ITERATION_NUMBERS{
                     self.locked = true
                 }
             }
@@ -93,7 +129,8 @@ class Module1ViewController: UIViewController {
                 self.lastMaxIndex2 = peaks[1]
                 self.noChangeCount = 0
             }
-        }
+        }*/
+        
     }
 
     /*
